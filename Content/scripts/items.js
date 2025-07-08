d52eb67cd6dea42857f6c41630f5928fae7d9a77
Devin <devini554@gmail.com>
2025-07-08 16:13:01 -0500

    Added more items to item page.

--- Content/scripts/items.js
+++ Content/scripts/items.js
@@ -11,23 +11,25 @@ document.querySelectorAll('.itemDiv').forEach(div => {
 
     button.addEventListener('click', () => {
         text.style.opacity = 0;
+        name.style.opacity = 0;
 
         setTimeout(() => {
             toggled = !toggled;
 
             if (toggled) {
-                div.style.backgroundColor = 'darkred';
+                div.style.border = '3px solid purple';
                 name.innerHTML = enhName;
                 text.innerHTML = enhText;
                 button.textContent = 'Show Regular';
             } else {
-                div.style.backgroundColor = 'black';
+                div.style.border = '3px solid white';
                 name.innerHTML = regName;
                 text.innerHTML = regText;
                 button.textContent = 'Show Enhanced';
             }
 
             text.style.opacity = 1;
-        }, 200);
+            name.style.opacity = 1;
+        }, 300);
     });
 });
