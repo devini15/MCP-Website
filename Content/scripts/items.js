572b9b40de06b5dae4083d1d5ae5371c2a884062
Devin <devini554@gmail.com>
2025-07-08 13:02:53 -0500

    Item page formatting.

--- Content/scripts/items.js
+++ Content/scripts/items.js
@@ -4,24 +4,30 @@ document.querySelectorAll('.itemDiv').forEach(div => {
     const text = div.querySelector('.description');
     const button = div.querySelector('.itemButton');
 
-    const regText = div.getAttribute('regular-text');
-    const enhText = div.getAttribute('enhanced-text');
+    const regText = div.getAttribute('regular-text').replace(/\\n/g, '<br/>');
+    const enhText = div.getAttribute('enhanced-text').replace(/\\n/g, '<br/>');
     const regName = div.getAttribute('regular-name');
     const enhName = div.getAttribute('enhanced-name');
 
     button.addEventListener('click', () => {
-        toggled = !toggled;
+        text.style.opacity = 0;
 
-        if (toggled) {
-            div.style.backgroundColor = 'darkred';
-            name.textContent = enhName;
-            text.textContent = enhText;
-            button.textContent = 'Show Regular';
-        } else {
-            div.style.backgroundColor = 'black';
-            name.textContent = regName;
-            text.textContent = regText;
-            button.textContent = 'Show Enhanced';
-        }
+        setTimeout(() => {
+            toggled = !toggled;
+
+            if (toggled) {
+                div.style.backgroundColor = 'darkred';
+                name.innerHTML = enhName;
+                text.innerHTML = enhText;
+                button.textContent = 'Show Regular';
+            } else {
+                div.style.backgroundColor = 'black';
+                name.innerHTML = regName;
+                text.innerHTML = regText;
+                button.textContent = 'Show Enhanced';
+            }
+
+            text.style.opacity = 1;
+        }, 200);
     });
 });
